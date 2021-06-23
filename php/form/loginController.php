ed0e70f10875913713d16f8eb76a1ad4da214565
enzo.tessonnieres <enzo.tessonnieres@outlook.fr>
2021-06-23 09:01:38 +0200

    adding loginController logic

--- php/form/loginController.php
+++ php/form/loginController.php
@@ -1,48 +1,70 @@
 <?php
-require ('php/datamanager/datamanager.php');
-mb_internal_encoding( "UTF-8" );
+require 'datamanager.php';
 
-function html($str): string
-{
-    return
-        htmlspecialchars(trim($str), ENT_QUOTES);
-}
+$login = $_POST['login'];
+$password = $_POST['password'];
 
-function mb_ucFirst($string): string
-{
-    $string = mb_strtolower($string);
-    return mb_strtoupper(mb_substr( $string, 0, 1 )) . mb_substr( $string, 1 );
-}
-
-$fields_required = array($_POST['pseudo'], $_POST['mdp']);
-if(in_array('', $fields_required)) {
-    $msg_error = "merci de remplir tous les champs";
-} else {
-    $pseudo = html($_POST['pseudo']);
-    $mdp = html($_POST['mdp']);
-
-    if (isset($_GET['id'])) {
-        $id = html($_GET['id']);
-        $data = array(
-            'id' => $id,
-            'pseudo' => $pseudo,
-            'mdp' => $mdp
-        );
-        $return = update($data);
-    } else {
-        $data = array(
-            'pseudo' => $pseudo,
-            'mdp' => $mdp
-        );
-        $return = add($data);
+if(in_array('', $_POST)) {
+    $msg_error = '';
+    if(empty($login)) {
+        $msg_error .= 'Merci de renseigner votre login<br>';
     }
-    if($return) {
-        $msg = "utilisateur bien crée";
-        $error = 'false';
+    if(empty($password)) {
+        $msg_error .= 'Merci de renseigner votre mot de passe<br>';
+    }
+}
+else {
+    $login = htmlentities(trim(mb_strtolower($login)), ENT_QUOTES); // faille XSS
+    $password = htmlentities(trim($password), ENT_QUOTES);
+
+    $req = $db->prepare("
+		SELECT *
+		FROM user u
+		INNER JOIN role r
+		ON u.id_role = r.id
+		WHERE u.email = :email
+	");
+    $req->bindValue(':email', $login, PDO::PARAM_STR);
+
+    $req->execute();
+
+    $result = $req->fetchObject();
+    if(!$result) {
+        $msg_error = 'Votre login ou mot de passe est inconnu';
     }
     else {
-        $msg = "Oups, une erreur s'est produite";
-        $error = 'true';
+
+        if(password_verify($password, $result->password)) {
+            $msg_success = 'Vous êtes connecté';
+        }
+        else {
+            $msg_error = 'Votre login ou mot de passe est inconnu';
+        }
     }
-    header("Location: admin?msg=$msg&error=$error");
 }
+
+$msg = isset($msg_error);
+
+$last_url = $_SERVER['HTTP_REFERER']; // url d'où je viens
+if(strpos($last_url, '?') !== FALSE) {
+    $req_get = strrchr($last_url, '?');
+    $last_url = str_replace($req_get, '', $last_url);
+}
+if($msg) {
+    header("Location: $last_url?msg=$msg_error");
+}
+else {
+
+    $_SESSION['firstname'] 	= $result->firstname;
+    $_SESSION['lastname'] 	= $result->lastname;
+    $_SESSION['id'] 		= $result->id;
+    $_SESSION['role'] 		= $result->id_role;
+    $_SESSION['role_name'] 	= $result->role_name;
+    require __DIR__ . '/last_connect.php';
+    $_SESSION['last_connect'] 	= $result->last_connect;
+
+
+    require __DIR__ . '/new_connect.php';
+    header("Location: $last_url?msg=$msg_success");
+}
+?>
